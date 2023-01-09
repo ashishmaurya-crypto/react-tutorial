cd0a021bfe1f7b6a9c5eebd32e3e97934f4bed9d
Ashish maurya <62279600+ashishmaurya-crypto@users.noreply.github.com>
2023-01-09 19:39:17 +0530

    redux tutorial

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import store from './store'
 import { Provider } from 'react-redux';
+import PersistedStore from './persistedStore';
 
 store.subscribe(()=> console.log(store.getState()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={PersistedStore.getDefaultStore().store}>
       <App />
     </Provider>
   </React.StrictMode>
