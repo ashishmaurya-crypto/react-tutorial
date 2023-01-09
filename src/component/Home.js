883a237646cec1fc31e396b49d4ba088cf964026
Ashish maurya <62279600+ashishmaurya-crypto@users.noreply.github.com>
2023-01-09 19:51:05 +0530

    add topics

--- src/component/Home.js
+++ src/component/Home.js
@@ -13,6 +13,13 @@ export default function Home() {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/routinghome">routing-tutorial</Link></li>
                     <li><Link to="/reduxhome">redux-tutorial</Link></li>
+                    <li><Link to="/">all-Input-tutorial</Link></li>
+                    <li><Link to="/">callback-conditional argument-tutorial</Link></li>
+                    <li><Link to="/">map-tutorial</Link></li>
+                    <li><Link to="/">filter-tutorial</Link></li>
+                    <li><Link to="/">array-tutorial</Link></li>
+                    <li><Link to="/">object-tutorial</Link></li>
+                    <li><Link to="/">api-integration-tutorial</Link></li>
                 </ul>
             </nav>
         </>
